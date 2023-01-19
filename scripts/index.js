5272c0e5e6bcc05201faf46e454dab68af83c76c
Lepso Vistav <hustlerleps@yandex.ru>
2023-01-19 14:36:22 +0300

    first try

--- scripts/index.js
+++ scripts/index.js
@@ -1,152 +1,154 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+
 const popupElement = document.querySelector('.popup')
-const popupAddElement = document.querySelector('.popup_add-element')
-const popupAddCloseButton = document.querySelector('.popup__close')
-const popupAddOpenButton = document.querySelector('.profile__add-button')
-let userName = 'Жак-Ив Кусто'
-let userJob = 'Исследователь океана'
-const postsWrapper = document.querySelector('#posts')
-const galleryWrapper = document.querySelector('#gallery')
+const popupEditElement = document.getElementById('popup-edit-profile')
+const popupOpenEditButton = document.querySelector('.profile__edit-button')
+const popupCloseEditButton = popupEditElement.querySelector('.popup__close')
+const popupAddElement = document.getElementById('popup-add-post')
+const popupOpenAddButton = document.querySelector('.profile__add-button')
+const popupCloseAddButton = popupAddElement.querySelector('.popup__close')
+const popupAddSaveButton = popupAddElement.querySelector('.popup__save')
+const popupImage = document.getElementById('popup-image')
+const popupCloseImageButton = popupImage.querySelector('.popup__close')
 
-const posts = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-      like: false
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-      like: false
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-      like: false
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-      like: false
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-      like: false
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-      like: false
-    }
-  ]; 
+let nameInput = popupEditElement.querySelector('#edit-profile-form input[name="name"]')
+let jobInput = popupElement.querySelector('#edit-profile-form input[name="job"]')
+const formEditElement = popupElement.querySelector('#edit-profile-form')
+let nameProfile = document.querySelector('.profile__title')
+let jobProfile = document.querySelector('.profile__subtitle')
 
+const openPopup = (popup) => {
+  popup.classList.add('popup_open')
+}
 
-function openModal(element) {
-    const modalWindow = document.querySelector(element)
-    modalWindow.classList.add('popup_open')
-    render() 
+const openImagePopup = function() {
+  openPopup(popupImage)
+}
+
+const openEditPopup = function() {
+  nameInput.value = nameProfile.textContent
+  jobInput.value = jobProfile.textContent
+  openPopup(popupEditElement)
+}
+
+const openAddPopup = function() {
+  openPopup(popupAddElement)
+}
+
+const closePopup = (popup) => {
+  popup.classList.remove('popup_open')
+}
+
+const closeImagePopup = function() {
+  closePopup(popupImage)
+}
+
+const closeEditPopup = function() {
+  closePopup(popupEditElement)
+}
+
+const closeAddPopup = function() {
+  closePopup(popupAddElement)
+}
+
+const openCardPopup = function() {
+  openPopup()
+}
+
+function handleEditFormSubmit(evt) {
+  evt.preventDefault()
+  nameProfile.textContent = nameInput.value
+  jobProfile.textContent = jobInput.value
+  closePopup(popupEditElement)
+}
+
+function createImage(name, link) {
+  const image = document.querySelector('.popup__image');
+  const imageSubtitle = document.querySelector('.popup__image-title')
+  image.src = link;
+  image.alt = name;
+  imageSubtitle.textContent = name;
+}
+
+const сardsContainer = document.querySelector('.elements__list')
+
+function createCard({ name, link}) {
+  const сardTemplate = document.querySelector('#initial-cards-template').content.querySelector('.element')
+  const card = сardTemplate.cloneNode(true)
+  const cardTitle = card.querySelector('.element__title')
+  cardTitle.textContent = name
+  const cardPic = card.querySelector('.element__pic')
+  cardPic.src = link
+  card.querySelector('.popup__delete-btn').addEventListener('click', () => {
+    card.remove()
+  })
+ const likeButton = card.querySelector('.element__like-button')
+  card.querySelector('.element__like-button').addEventListener('click', () => {
+    likeButton.classList.toggle('element__like-button_active') 
+  })
+  cardPic.addEventListener('click', () => {
+    createImage(name, link)
+    openPopup(popupImage)
+  })
+  return card
 }
 
 
-function openImage(postId) {
-    const openImageLink = posts[postId].link
-    const openImageName =  posts[postId].name
-    const galleryHtml = `
-      <img src="${openImageLink}" class="popup__image" alt="${openImageName}">
-      <p class="popup__image-title">${openImageName}</p>
-      <button class="popup__close" type="button" onclick="closeModal('#popup-image-container')"></button>
-    `
-    galleryWrapper.innerHTML =  galleryHtml
-    openModal('#popup-image-container')
+
+function renderCards() {
+  initialCards.forEach((card , index) => {
+    const cardHtml = createCard(card, index)
+    сardsContainer.prepend(cardHtml)
+  })
+}
+renderCards()
+
+let formAddElement = document.querySelector('#add-post-form')
+let nameAddInput = popupAddElement.querySelector('#add-post-form input[name="postName"]')
+let linkAddInput = popupAddElement.querySelector('#add-post-form input[name="postLink"]')
+
+
+function handleAddFormSubmit(evt) {
+  evt.preventDefault()
+  const newCard = createCard({
+    name: nameAddInput.value, 
+    link: linkAddInput.value
+  })
+  сardsContainer.prepend(newCard)
+  closePopup(popupAddElement)
 }
 
 
-
-function closeModal(element) {
-    const modalWindow = document.querySelector(element)
-    modalWindow.classList.remove('popup_open')
-}
-
-
-const editProfileForm = document.querySelector('#edit-profile-form')
-const nameInput = document.querySelector('#edit-profile-form input[name="name"]')
-const jobInput = document.querySelector('#edit-profile-form input[name="job"]')
-const addPostForm = document.querySelector('#add-post-form')
-const postNameInput = document.querySelector('#add-post-form input[name="postName"]')
-const postLinkInput = document.querySelector('#add-post-form input[name="postLink"]')
-
-
-
-function addPost(evt) {
-    evt.preventDefault(); 
-    const newPost = {
-        name: postNameInput.value ,
-        link: postLinkInput.value ,
-        like: false
-      };
-    posts.unshift(newPost)  
-    render()
-    closeModal('#popup-add-post')
-    postNameInput.value = ''
-    postLinkInput.value = ''
-}
-
-addPostForm.addEventListener('submit', addPost); 
-
-function saveProfile(evt) {
-    evt.preventDefault();
-    userName = nameInput.value 
-    userJob = jobInput.value 
-    render()
-    closeModal('#popup-edit-profile')
-}
-
-editProfileForm.addEventListener('submit', saveProfile); 
-
-
-
-const userNameElement = document.querySelector('.profile__title')
-const userJobElement = document.querySelector('.profile__subtitle')
-
-function render () {
-    
-    userNameElement.textContent = userName
-    userJobElement.textContent = userJob
-    nameInput.value = userName
-    jobInput.value = userJob
-    
-    let postListHtml = '';
-    if (posts.length === 0) {
-        postListHtml = `<p>Посты отсутствуют</p>`
-    } else {
-        posts.forEach((post, key) => {
-            let likeClass = post.like ? 'element__like-button_active' : '' 
-            postListHtml += `
-            <div class="element">
-              <img src="${post.link}" alt="${post.name}" class="element__pic" onclick="openImage(${key})">
-              <div class="element__like-container">
-                <h3 class="element__title">${post.name}</h3>
-                <button class="element__like-button ${likeClass}" type="button" onclick="likePost(${key})"></button>
-              </div>  
-              <button class="element__delete-button" type="button" onclick="removePost(${key})"></button>
-            </div>`
-        })
-    }
-    postsWrapper.innerHTML = postListHtml
-}
-
-function removePost(postId) {
-    posts.splice(postId, 1)
-    render()
-}
-
-function likePost(postId) {
-    posts[postId].like = !posts[postId].like
-    render()
-}
-
-render()
-
-const popupImageContainer = document.querySelector('#popup-image-container')
-const popupImage = document.querySelector('.popup__image')
-const popupImageTitle = document.querySelector('.popup__image-title')
+popupCloseImageButton.addEventListener('click', closeImagePopup)
+popupAddElement.addEventListener('submit', handleAddFormSubmit)
+formEditElement.addEventListener('submit', handleEditFormSubmit)
+popupOpenEditButton.addEventListener('click', openEditPopup)
+popupCloseEditButton.addEventListener('click', closeEditPopup)
+popupOpenAddButton.addEventListener('click', openAddPopup)
+popupCloseAddButton.addEventListener('click', closeAddPopup)
