5e95f9647c9036b41098b20c8c19caaa16e0bf48
Lepso Vistav <hustlerleps@yandex.ru>
2023-05-13 13:03:07 +0300

    first try

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,11 @@
 export default class Card {
-    constructor({name, link}, templateSelector, handleCardClick) {
+    constructor({name, link, likes, owner, _id}, templateSelector, handleCardClick, handleCardDeleteClick, userId, handleLikeClick) {
         this._name = name;
         this._link = link;
+        this._likes = likes;
+        this._owner = owner;
+        this.cardId = _id; 
+        this._userId = userId;
         this._templateSelector = templateSelector;
         this._handleCardClick = handleCardClick;
         this._card = this._getTemplate();
@@ -11,7 +15,10 @@ export default class Card {
         this._deleteButton = this._card.querySelector('.element__delete-btn');
         this._imageButton = this._card.querySelector('.element__image-btn');
         this._likeCard = this._likeCard.bind(this);
-        this._deleteCard = this._deleteCard.bind(this);
+        this._handleCardDeleteClick = handleCardDeleteClick;
+        this._likeCounter = this._card.querySelector('.element__like-counter');
+        this._isOwn = userId === owner._id;
+        this._handleLikeClick = handleLikeClick;
     }
 
     _getTemplate() {
@@ -22,15 +29,35 @@ export default class Card {
         this._cardName.textContent = this._name; 
         this._cardImage.alt = this._name;
         this._cardImage.src = this._link;
+        
+        if (!this._isOwn) {
+            this._removeTrashButton();
+        }
 
         this._setEventListeners();
+
+        this.setLike()
 
         return this._card;
     }
 
+    isLiked() {
+        return this._likes.some(like => like._id === this._userId);
+    }
+
+    setLike() {
+        this._likeCounter.textContent = this._likes.length;
+        if (this.isLiked()) {
+            this._likeButton.classList.add('element__like-button_active');
+        } else {
+            this._likeButton.classList.remove('element__like-button_active');
+
+        }
+    }
+   
     _setEventListeners() {
-        this._likeButton.addEventListener('click', this._likeCard);
-        this._deleteButton.addEventListener('click', this._deleteCard);
+        this._likeButton.addEventListener('click',() => this._handleLikeClick(this.cardId, this.isLiked(), this));
+        this._deleteButton.addEventListener('click',() => this._handleCardDeleteClick(this));
         this._imageButton.addEventListener('click',() => this._handleCardClick(this._link, this._name));
     }
 
@@ -38,7 +65,20 @@ export default class Card {
         this._likeButton.classList.toggle('element__like-button_active');
     }
 
-    _deleteCard() {
+    deleteCard() {
         this._card.remove();
+    }
+
+    _removeTrashButton() {
+        this._deleteButton.remove();
+    }
+
+    updateLikesInfo(likes) {
+        this._likes = likes;
+        this.setLike();
+    }
+
+    getCardId() {
+        return this.cardId;
     }
 }
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,15 +1,20 @@
 export default class UserInfo {
-    constructor({userNameSelector, userJobSelector}) {
+    constructor({userNameSelector, userJobSelector, userAvatarSelector}) {
         this._userName = document.querySelector(userNameSelector)
         this._userJob = document.querySelector(userJobSelector)
+        this._userAvatar = document.querySelector(userAvatarSelector)
     }
 
     getUserInfo() {
-        return {name: this._userName.textContent, job: this._userJob.textContent,};
+        return {name: this._userName.textContent, about: this._userJob.textContent};
     }
 
     setUserInfo(userData) {
         this._userName.textContent = userData.name;
-        this._userJob.textContent = userData.job;
+        this._userJob.textContent = userData.about;
+    }
+
+    setAvatarInfo(userData) {
+        this._userAvatar.src = userData.avatar;
     }
 }
